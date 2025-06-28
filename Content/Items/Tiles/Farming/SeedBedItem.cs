6bd0dc19134170a4fd66821344f895dbf50d3764
wymsical <65696435+itswymsical@users.noreply.github.com>
2025-06-28 16:25:09 -0700

    seedbed localization + item sprite

--- Content/Items/Tiles/Farming/SeedBedItem.cs
+++ Content/Items/Tiles/Farming/SeedBedItem.cs
@@ -1,17 +1,12 @@
 ﻿using Reverie.Content.Tiles.Farming;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Reverie.Content.Items.Tiles.Farming;
 
 public class SeedBedItem : ModItem
 {
-    public override string Texture => PLACEHOLDER;
     public override void SetDefaults()
     {
         Item.DefaultToPlaceableTile(ModContent.TileType<SeedBedTile>());
+        Item.value = Item.sellPrice(copper: 78);
     }
 }
