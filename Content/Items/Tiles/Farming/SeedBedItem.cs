5814f6153b10b88cb355e971dd319236b08a5484
wymsical <65696435+itswymsical@users.noreply.github.com>
2025-06-28 16:22:25 -0700

    feat: seedbed

---
+++ Content/Items/Tiles/Farming/SeedBedItem.cs
@@ -0,0 +1,17 @@
+﻿using Reverie.Content.Tiles.Farming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverie.Content.Items.Tiles.Farming;
+
+public class SeedBedItem : ModItem
+{
+    public override string Texture => PLACEHOLDER;
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<SeedBedTile>());
+    }
+}
---
+++ Content/Tiles/Farming/SeedBedTile.cs
@@ -0,0 +1,194 @@
+﻿using Reverie.Content.Tiles.Canopy;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Metadata;
+using Terraria.Localization;
+using Terraria.ObjectData;
+
+namespace Reverie.Content.Tiles.Farming;
+
+public class SeedBedTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = false;
+        Main.tileSolidTop[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+        Main.tileBlockLight[Type] = false;
+        Main.tileLighted[Type] = false;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+
+        TileObjectData.newTile.CoordinateWidth = 18;
+        TileObjectData.newTile.CoordinateHeights = [16];
+        TileObjectData.newTile.DrawYOffset = -(16 - 18);
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, 1, 0);
+
+        AdjTiles = [TileID.PlanterBox, TileID.ClayPot];
+
+        TileObjectData.newTile.AnchorInvalidTiles = [TileID.MagicalIceBlock];
+        TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+        TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Plant"]);
+
+        AddMapEntry(new Color(247, 124, 124), Language.GetText("Mods.Reverie.Tiles.SeedbedTile.MapEntry"));
+        DustType = DustID.Dirt;
+        HitSound = SoundID.Dig;
+
+        TileObjectData.addTile(Type);
+    }
+
+    public override bool CanPlace(int i, int j)
+    {
+        var tileBelow = Framing.GetTileSafely(i, j + 1);
+        return tileBelow.HasTile && (Main.tileSolid[tileBelow.TileType] || tileBelow.TileType == Type) &&
+               !tileBelow.IsHalfBlock && tileBelow.Slope == 0;
+    }
+
+    public override void PostSetDefaults()
+    {
+        Main.tileNoSunLight[Type] = false;
+    }
+
+    public override void PlaceInWorld(int i, int j, Item item)
+    {
+        UpdateFraming(i, j);
+    }
+
+    public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+    {
+        if (!fail)
+        {
+            UpdateFraming(i - 1, j); // Left side
+            UpdateFraming(i + 1, j); // Right side
+        }
+    }
+
+    //TODO: update framing when a center frame becomes an end frame
+    void UpdateFraming(int centerI, int centerJ)
+    {
+        // Skip if no seedbed at this position
+        if (!IsSeedbed(centerI, centerJ))
+            return;
+
+        // Find the full extent of connected seedbeds horizontally
+        int leftMost = centerI;
+        int rightMost = centerI;
+
+        // Find leftmost seedbed
+        while (IsSeedbed(leftMost - 1, centerJ))
+            leftMost--;
+
+        // Find rightmost seedbed
+        while (IsSeedbed(rightMost + 1, centerJ))
+            rightMost++;
+
+        // Update all tiles in the sequence
+        for (int x = leftMost; x <= rightMost; x++)
+        {
+            if (IsSeedbed(x, centerJ))
+            {
+                UpdateTileFrame(x, centerJ, leftMost, rightMost);
+            }
+        }
+    }
+
+    void UpdateTileFrame(int i, int j, int leftMost, int rightMost)
+    {
+        var tile = Framing.GetTileSafely(i, j);
+        if (!tile.HasTile || tile.TileType != Type)
+            return;
+
+        int sequenceLength = rightMost - leftMost + 1;
+        int positionInSequence = i - leftMost;
+
+        int frameX;
+
+        if (sequenceLength == 1)
+        {
+            // Orphan tile
+            frameX = 7;
+        }
+        else if (positionInSequence == 0)
+        {
+            // Left end
+            frameX = 0;
+        }
+        else if (positionInSequence == sequenceLength - 1)
+        {
+            // Right end
+            frameX = 6;
+        }
+        else
+        {
+            // Calculate center positions
+            int centerPos = sequenceLength / 2;
+
+            if (positionInSequence == centerPos && sequenceLength % 2 == 1)
+            {
+                // True center - only for odd-length sequences at exact middle
+                frameX = 3;
+            }
+            else if (positionInSequence < centerPos)
+            {
+                // Left side centers - cycle through frames 1-2
+                int leftCenterIndex = (positionInSequence - 1) % 2;
+                frameX = 1 + leftCenterIndex;
+            }
+            else
+            {
+                // Right side centers - cycle through frames 4-5
+                int rightCenterIndex = (positionInSequence - centerPos - (sequenceLength % 2)) % 2;
+                frameX = 4 + rightCenterIndex;
+            }
+        }
+
+        short newFrameX = (short)(frameX * 18);
+        if (tile.TileFrameX != newFrameX)
+        {
+            tile.TileFrameX = newFrameX;
+            tile.TileFrameY = 0;
+
+            if (Main.netMode != NetmodeID.SinglePlayer)
+            {
+                NetMessage.SendTileSquare(-1, i, j, 1, 1, TileChangeType.None);
+            }
+        }
+    }
+
+    bool IsSeedbed(int i, int j)
+    {
+        var tile = Framing.GetTileSafely(i, j);
+        return tile.HasTile && tile.TileType == Type;
+    }
+
+    public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+    {
+        var tileBelow = Framing.GetTileSafely(i, j + 1);
+        if (!tileBelow.HasTile || (!Main.tileSolid[tileBelow.TileType] && tileBelow.TileType != Type) ||
+            tileBelow.IsHalfBlock || tileBelow.Slope != 0)
+        {
+            WorldGen.KillTile(i, j);
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void RandomUpdate(int i, int j)
+    {
+        var tile = Framing.GetTileSafely(i, j);
+        if (!tile.HasTile || tile.TileType != Type)
+            return;
+
+        if (WorldGen.genRand.NextBool(20))
+        {
+            var dustPos = new Vector2(i * 16 + 8, j * 16 + 8);
+            var dust = Dust.NewDustDirect(dustPos, 0, 0, DustID.Clay,
+                Main.rand.NextFloat(-0.5f, 0.5f), Main.rand.NextFloat(-1f, 0f));
+            dust.fadeIn = 0.8f;
+            dust.scale = 0.6f;
+        }
+    }
+}
