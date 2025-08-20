59cd5c867c2917c7cb9ea34ab734e2c32b6b8e6a
wymsical <65696435+itswymsical@users.noreply.github.com>
2025-08-20 07:37:16 -0700

    feat: furniture base classes

---
+++ Core/Items/Components/FurnitureComponent.cs
@@ -0,0 +1,156 @@
+﻿using Reverie.Core.Tiles.Actors;
+
+namespace Reverie.Core.Items.Components;
+
+public abstract class FurnitureItem : ModItem
+{
+    protected abstract FurnitureType FurnitureType { get; }
+    protected abstract int TileType { get; }
+    protected abstract int MaterialType { get; }
+    protected virtual int TorchType { get; }
+
+    public override void SetDefaults()
+    {
+        Item.value = 150;
+        Item.DefaultToPlaceableTile(TileType);
+    }
+
+    public override void AddRecipes()
+    {
+        var recipe = CreateRecipe();
+
+        switch (FurnitureType)
+        {
+            case FurnitureType.Chair:
+                recipe.AddIngredient(MaterialType, 4);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Table:
+                recipe.AddIngredient(MaterialType, 8);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Workbench:
+                recipe.AddIngredient(MaterialType, 10);
+                break;
+
+            case FurnitureType.Bed:
+                recipe.AddIngredient(MaterialType, 15);
+                recipe.AddIngredient(ItemID.Silk, 5);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Dresser:
+                recipe.AddIngredient(MaterialType, 16);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Sink:
+                recipe.AddIngredient(MaterialType, 6);
+                recipe.AddIngredient(ItemID.WaterBucket);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Bathtub:
+                recipe.AddIngredient(MaterialType, 14);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Bookcase:
+                recipe.AddIngredient(MaterialType, 20);
+                recipe.AddIngredient(ItemID.Book, 10);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Piano:
+                recipe.AddIngredient(ItemID.Bone, 4);
+                recipe.AddIngredient(MaterialType, 15);
+                recipe.AddIngredient(ItemID.Book);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Lamp:
+                recipe.AddIngredient(MaterialType, 3);
+                recipe.AddIngredient(ItemID.Torch);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Sofa:
+                recipe.AddIngredient(MaterialType, 5);
+                recipe.AddIngredient(ItemID.Silk, 2);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Chandelier:
+                recipe.AddIngredient(MaterialType, 4);
+                recipe.AddIngredient(ItemID.Torch, 4);
+                recipe.AddIngredient(ItemID.Chain);
+                recipe.AddTile(TileID.Anvils);
+                break;
+
+            case FurnitureType.Candelabra:
+                recipe.AddIngredient(MaterialType, 5);
+                recipe.AddIngredient(ItemID.Torch, 5);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Lantern:
+                recipe.AddIngredient(MaterialType, 6);
+                recipe.AddIngredient(ItemID.Torch, 1);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Clock:
+                recipe.AddIngredient(MaterialType, 10);
+                recipe.AddIngredient(ItemID.Glass, 6);
+                recipe.AddRecipeGroup(RecipeGroupID.IronBar, 3);
+                recipe.AddTile(TileID.Sawmill);
+                break;
+
+            case FurnitureType.Chest:
+                recipe.AddIngredient(MaterialType, 8);
+                recipe.AddRecipeGroup(RecipeGroupID.IronBar, 2);
+                recipe.AddTile(TileID.WorkBenches);
+                break;
+
+            case FurnitureType.Campfire:
+                recipe.AddIngredient(MaterialType, 10);
+                recipe.AddIngredient(TorchType, 5);
+                break;
+
+            case FurnitureType.Platform:
+                recipe = CreateRecipe(2);
+                recipe.AddIngredient(MaterialType);
+                break;
+
+            case FurnitureType.Torch:
+                recipe = CreateRecipe(3);
+                recipe.AddIngredient(MaterialType);
+                recipe.AddIngredient(ItemID.Gel);
+                break;
+        }
+
+        recipe.Register();
+    }
+}
+
+public abstract class DoorItem : ModItem
+{
+    protected abstract int ClosedDoorType { get; }
+    protected abstract int MaterialType { get; }
+
+    public override void SetDefaults()
+    {
+        Item.value = 150;
+        Item.DefaultToPlaceableTile(ClosedDoorType);
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(MaterialType, 6)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
---
+++ Utilities/Extensions/TileExtensions.cs
@@ -0,0 +1,344 @@
+﻿/*
+ * Copyright (C) 2025 Spirit Reforged
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.Drawing;
+using Terraria.ObjectData;
+
+namespace Reverie.Utilities.Extensions;
+
+public static class TileExtensions
+{
+	/// <summary> Gets common visual info related to the tile at the given coordinates, such as painted color. </summary>
+	/// <param name="i"> The X coordinate. </param>
+	/// <param name="j"> The Y coordinate.</param>
+	/// <param name="color"> The color of the tile affected by coatings. </param>
+	/// <param name="texture"> The default tile texture, painted. </param>
+	/// <returns> Whether the tile should be drawn based on <see cref="TileDrawing.IsVisible"/>. </returns>
+	public static bool GetVisualInfo(int i, int j, out Color color, out Texture2D texture)
+	{
+		var t = Main.tile[i, j];
+		color = t.IsTileFullbright ? Color.White : Lighting.GetColor(i, j);
+		texture = TextureAssets.Tile[t.TileType].Value;
+
+		if (!TileDrawing.IsVisible(t))
+			return false;
+
+		if (t.TileColor != PaintID.None)
+		{
+			var painted = Main.instance.TilePaintSystem.TryGetTileAndRequestIfNotReady(t.TileType, 0, t.TileColor);
+			texture = painted ?? texture;
+		}
+
+		return true;
+	}
+
+	/// <summary> Gets a tint based on the paint type at the given coordinates.<br/>
+	/// Useful for coloring non-default tile textures, like glowmasks. Otherwise, <see cref="GetVisualInfo"/> should be used. </summary>
+	/// <param name="i"> The X coordinate. </param>
+	/// <param name="j"> The Y coordinate.</param>
+	/// <param name="color"> The color to tint. </param>
+	public static Color GetTint(int i, int j, Color color)
+	{
+		var t = Main.tile[i, j];
+		int type = Main.tile[i, j].TileColor;
+		var paint = WorldGen.paintColor(type);
+
+		if (t.IsTileFullbright)
+			color = Color.White;
+		else if (type is >= 13 and <= 24) //Deep paints
+			color = GetIntensity(1f);
+		else
+			color = GetIntensity(0.5f);
+
+		return color;
+
+		Color GetIntensity(float value) => color.MultiplyRGB(Color.Lerp(Color.White, paint, value));
+	}
+
+	public static Color GetSpelunkerTint(Color color)
+	{
+		if (color.R < 200)
+			color.R = 200;
+
+		if (color.G < 170)
+			color.G = 170;
+
+		return color;
+	}
+
+	public static Vector2 TileOffset => Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+	public static Vector2 DrawPosition(this ModTile _, int i, int j, Vector2 off = default) => DrawPosition(i, j, off);
+	public static Vector2 DrawPosition(int i, int j, Vector2 off = default) => new Vector2(i, j) * 16 - Main.screenPosition - off + TileOffset;
+
+	public static void DrawSloped(this ModTile _, int i, int j, Texture2D texture, Color color, Vector2 positionOffset, bool overrideFrame = false)
+		=> DrawSloped(i, j, texture, color, positionOffset, overrideFrame);
+
+	public static void DrawSloped(int i, int j, Texture2D texture, Color color, Vector2 positionOffset, bool overrideFrame = false)
+	{
+		var tile = Main.tile[i, j];
+		int frameX = tile.TileFrameX;
+		int frameY = tile.TileFrameY;
+
+		if (overrideFrame)
+		{
+			frameX = 0;
+			frameY = 0;
+		}
+
+		var width = 16;
+		var height = 16;
+		var location = new Vector2(i * 16, j * 16);
+		var offsets = -Main.screenPosition + TileOffset + positionOffset;
+		var drawLoc = location + offsets;
+
+		if (tile.Slope == 0 && !tile.IsHalfBlock || Main.tileSolid[tile.TileType] && Main.tileSolidTop[tile.TileType]) //second one should be for platforms
+			Main.spriteBatch.Draw(texture, drawLoc, new Rectangle(frameX, frameY, width, height), color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		else if (tile.IsHalfBlock)
+			Main.spriteBatch.Draw(texture, new Vector2(drawLoc.X, drawLoc.Y + 8), new Rectangle(frameX, frameY, width, 8), color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		else
+		{
+			var b = tile.Slope;
+			Rectangle frame;
+			Vector2 drawPos;
+
+			if (b is SlopeType.SlopeDownLeft or SlopeType.SlopeDownRight)
+			{
+				int length;
+				int height2;
+
+				for (var a = 0; a < 8; ++a)
+				{
+					if (b == SlopeType.SlopeDownRight)
+					{
+						length = 16 - a * 2 - 2;
+						height2 = 14 - a * 2;
+					}
+					else
+					{
+						length = a * 2;
+						height2 = 14 - length;
+					}
+
+					frame = new Rectangle(frameX + length, frameY, 2, height2);
+					drawPos = new Vector2(i * 16 + length, j * 16 + a * 2) + offsets;
+					Main.spriteBatch.Draw(texture, drawPos, frame, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+				}
+
+				frame = new Rectangle(frameX, frameY + 14, 16, 2);
+				drawPos = new Vector2(i * 16, j * 16 + 14) + offsets;
+				Main.spriteBatch.Draw(texture, drawPos, frame, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+			}
+			else
+			{
+				int length;
+				int height2;
+
+				for (var a = 0; a < 8; ++a)
+				{
+					if (b == SlopeType.SlopeUpLeft)
+					{
+						length = a * 2;
+						height2 = 16 - length;
+					}
+					else
+					{
+						length = 16 - a * 2 - 2;
+						height2 = 16 - a * 2;
+					}
+
+					frame = new Rectangle(frameX + length, frameY + 16 - height2, 2, height2);
+					drawPos = new Vector2(i * 16 + length, j * 16) + offsets;
+					Main.spriteBatch.Draw(texture, drawPos, frame, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+				}
+
+				drawPos = new Vector2(i * 16, j * 16) + offsets;
+				frame = new Rectangle(frameX, frameY, 16, 2);
+				Main.spriteBatch.Draw(texture, drawPos, frame, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+			}
+		}
+	}
+
+	/// <summary> Gets the top left tile in a multitile using the given coordinates. Useful for things like tile entities whos data is stored only in a single tile. <br/>
+	/// This method relies on tileFrame to get the tile and may not work depending on how those variables are used. </summary>
+	public static void GetTopLeft(ref int i, ref int j)
+	{
+		var tile = Framing.GetTileSafely(i, j);
+		var data = TileObjectData.GetTileData(tile);
+
+		if (data is null)
+			return;
+
+		(i, j) = (i - tile.TileFrameX % data.CoordinateFullWidth / 18, j - tile.TileFrameY % data.CoordinateFullHeight / 18);
+	}
+
+	/// <summary> Checks if the tile at i, j is a chest, and returns what kind of chest it is if so. </summary>
+	/// <param name="i">X position.</param>
+	/// <param name="j">Y position.</param>
+	/// <param name="type">The type of the chest, if any.</param>
+	/// <returns>If the tile is a chest or not.</returns>
+	public static bool TryGetChestID(int i, int j, out VanillaChestID type)
+	{
+		var tile = Main.tile[i, j];
+		type = VanillaChestID.Wood;
+
+		if (tile.HasTile && tile.TileType == TileID.Containers && tile.TileFrameX % 36 == 0 && tile.TileFrameY == 0)
+		{
+			type = (VanillaChestID)(tile.TileFrameX / 36);
+			return true;
+		}
+
+		return false;
+	}
+
+	/// <summary>
+	/// Quickly retrieves a given tile's data.
+	/// </summary>
+	/// <param name="tile">The tile to get data from.</param>
+	/// <returns>The tile data.</returns>
+	public static TileObjectData SafelyGetData(this Tile tile) => TileObjectData.GetTileData(tile);
+
+	/// <summary>
+	/// Mutually merges the given tile with all of the ids in <paramref name="otherIds"/>.
+	/// </summary>
+	/// <param name="tile">The tile to merge with.</param>
+	/// <param name="otherIds">All other tiles to merge with.</param>
+	public static void Merge(this ModTile tile, params int[] otherIds)
+	{
+		foreach (var id in otherIds)
+		{
+			Main.tileMerge[tile.Type][id] = true;
+			Main.tileMerge[id][tile.Type] = true;
+		}
+	}
+
+	/// <summary> Allows <paramref name="types"/> to anchor to this ModTile. </summary>
+	public static void AnchorSelfTo(this ModTile tile, params int[] types) => AnchorSelfTo(tile.Type, types);
+
+	/// <inheritdoc cref="AnchorSelfTo"/>
+	public static void AnchorSelfTo(int modTileType, params int[] types)
+	{
+		foreach (var type in types)
+		{
+			if (TileObjectData.GetTileData(type, 0) is TileObjectData data && data.AnchorValidTiles != null)
+				data.AnchorValidTiles = [.. data.AnchorValidTiles, modTileType];
+		}
+	}
+
+	public static Point16 GetAnchor(int i, int j)
+	{
+		var coords = Point16.Zero;
+
+		if (TileObjectData.GetTileData(Main.tile[i, j].TileType, 0) is TileObjectData data)
+		{
+			if (data.AnchorBottom != AnchorData.Empty && Valid(coords = new(i, j + 1), data))
+				return coords;
+
+			if (data.AnchorLeft != AnchorData.Empty && Valid(coords = new(i - 1, j), data))
+				return coords;
+
+			if (data.AnchorRight != AnchorData.Empty && Valid(coords = new(i + 1, j), data))
+				return coords;
+
+			if (data.AnchorTop != AnchorData.Empty && Valid(coords = new(i, j - 1), data))
+				return coords;
+		}
+
+		return coords;
+
+		static bool Valid(Point16 coords, TileObjectData data)
+		{
+			int type = Framing.GetTileSafely(coords).TileType;
+			return data.isValidTileAnchor(type) || data.isValidAlternateAnchor(type);
+		}
+	}
+}
+public enum VanillaChestID : byte
+{
+    Wood,
+    Gold,
+    GoldLocked,
+    Shadow,
+    ShadowLocked,
+    Barrel,
+    TrashCan,
+    Ebonwood,
+    Mahogany,
+    Pearlwood,
+    Ivy,
+    Frozen,
+    LivingWood,
+    Sky,
+    Shadewood,
+    Webbed,
+    Lihahzrd,
+    Water,
+    Jungle,
+    Corruption,
+    Crimson,
+    Hallow,
+    Ice,
+    JungleLocked,
+    CorruptionLocked,
+    CrimsonLocked,
+    HallowLocked,
+    IceLocked,
+    Dynasty,
+    Honey,
+    Steampunk,
+    PalmWood,
+    Mushroom,
+    BorealWood,
+    Slime,
+    DungeonGreen,
+    DungeonGreenLocked,
+    DungeonPink,
+    DungeonPinkLocked,
+    DungeonBlue,
+    DungeonBlueLocked,
+    Bone,
+    Cactus,
+    Flesh,
+    Obsidian,
+    Pumpkin,
+    Spooky,
+    Glass,
+    Martian,
+    Meteorite,
+    Granite,
+    Marble,
+    Crystal,
+    Golden
+}
+
+public enum VanillaChestID2 : byte
+{
+    Crystal,
+    Golden,
+    Spider,
+    Corrupt,
+    Trapped,
+    Solar,
+    Vortex,
+    Nebula,
+    Stardust,
+    Golf,
+    Sandstone,
+    Bamboo,
+    Desert,
+    DesertLocked
+}
